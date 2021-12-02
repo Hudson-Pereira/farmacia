be96e0b6785a2746248a231cfd29b9bbd51517fe
Hudson-Pereira <hudson.o.pereira@gmail.com>
2021-12-02 18:29:49 -0300

    criada filial

--- farmacia/src/app.module.ts
+++ farmacia/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MatrizModule } from './matriz/matriz.module';
+import { FilialModule } from './filial/filial.module';
 
 @Module({
-  imports: [MatrizModule],
+  imports: [MatrizModule, FilialModule],
   controllers: [AppController],
   providers: [AppService],
 })
