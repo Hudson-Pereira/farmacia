db01eb3fd80f4a7db3ae8cf1d5ddbf523bad74c2
Hudson-Pereira <hudson.o.pereira@gmail.com>
2021-12-03 19:05:23 -0300

    validacao produto

--- farmacia/src/produto/dto/create-produto.dto.ts
+++ farmacia/src/produto/dto/create-produto.dto.ts
@@ -1,9 +1,31 @@
+import { IsNotEmpty, IsString, IsInt, IsBoolean } from 'class-validator';
+
 export class CreateProdutoDto {
+  @IsNotEmpty()
+  @IsString()
   thumbnail: string;
+
+  @IsNotEmpty()
+  @IsString()
   nome: string;
+
+  @IsNotEmpty()
+  @IsInt()
   preco: number;
+
+  @IsNotEmpty()
+  @IsString()
   ingredientes: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
   disponibilidade: boolean;
+
+  @IsNotEmpty()
+  @IsInt()
   volume: number;
+
+  @IsNotEmpty()
+  @IsString()
   fabricante: string;
 }
